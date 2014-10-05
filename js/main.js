3d067885e050894791df59eeb10d7b990ed15ac1
therealvdeadline <therealvdeadline@gmail.com>
2014-10-05 21:31:07 +1100

    pls

--- js/main.js
+++ js/main.js
@@ -17,8 +17,10 @@ $(window).scroll(function() {
 
     if (scroll >= 10) {
         $(".navbar-default").addClass("navbar-bg");
+        $(".dropdown-menu").addClass("navbar-bg");
     } else {
         $(".navbar-default").removeClass("navbar-bg");
+        $(".dropdown-menu").removeClass("navbar-bg");
     }
 });
 
@@ -27,3 +29,22 @@ window.sr = new scrollReveal({ mobile: false })
 scrollReveal.init();
 });
 
+// ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  $('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  // ADD SLIDEUP ANIMATION TO DROPDOWN //
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+
+  // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  $('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.navbar-toggle').first().stop(true, true).slideDown();
+  });
+
+  // ADD SLIDEUP ANIMATION TO DROPDOWN //
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.navbar-toggle').first().stop(true, true).slideUp();
+  });
