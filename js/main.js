c131eb33d4ac26b0a4b5802a05039057d75afbf3
therealvdeadline <therealvdeadline@gmail.com>
2014-10-05 20:39:31 +1100

    ds
    d

--- js/main.js
+++ js/main.js
@@ -27,13 +27,3 @@ window.sr = new scrollReveal({ mobile: false })
 scrollReveal.init();
 });
 
-jQuery(document).ready(function($){
-$('.dropdown-toggle').click(function(e) {
-  e.preventDefault();
-  setTimeout($.proxy(function() {
-    if ('ontouchstart' in document.documentElement) {
-      $(this).siblings('.dropdown-backdrop').off().remove();
-    }
-  }, this), 0);
-});
-});
